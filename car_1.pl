e9be7bdce25ae1d46de07b36e3260f6d39b5d39c
Nikita Makarov <quiz.beat@gmail.com>
2015-06-17 01:30:55 +0300

    add hypothesizes for cars

--- car_1.pl
+++ car_1.pl
@@ -4,15 +4,103 @@ go :- hypothesize(Car),
 	  nl,
       undo.
 
-/* hypotheses to be tested */
+/******************************************/
+/*             Available cars             */
+/******************************************/
+hypothesize(ford_ka) :- ford_ka, !.
+hypothesize(peugeot_107) :- peugeot_107, !.
+
+hypothesize(chevrolet_aveo) :- chevrolet_aveo, !.
+hypothesize(peugeot_308) :- peugeot_308, !.
+hypothesize(opel_astra) :- opel_astra, !.
+hypothesize(toyota_auris) :- toyota_auris, !.
+
+hypothesize(ford_focus) :- ford_focus, !.
+hypothesize(peugeot_408) :- peugeot_408, !.
+hypothesize(volkswagen_jetta) :- volkswagen_jetta, !.
+hypothesize(toyota_corolla) :- toyota_corolla, !.
+
+hypothesize(chevrolet_cruze) :- chevrolet_cruze, !.
+hypothesize(peugeot_407) :- peugeot_407, !.
+hypothesize(opel_vectra) :- opel_vectra, !.
+hypothesize(toyota_prius) :- toyota_prius, !.
+
+hypothesize(ford_escape) :- ford_escape, !.
+hypothesize(citroen_cactus) :- citroen_cactus, !.
+hypothesize(opel_mokka) :- opel_mokka, !.
+hypothesize(nissan_juke) :- nissan_juke, !.
+
+hypothesize(renault_duster) :- renault_duster, !.
+
+
+hypothesize(mini_cooper) :- mini_cooper, !.
+hypothesize(smart_for_two) :- smart_for_two, !.
+hypothesize(toyota_iq) :- toyota_iq, !.
+
 hypothesize(audi_a1) :- audi_a1, !.
+hypothesize(subaru_impreza) :- subaru_impreza, !.
+hypothesize(volvo_c30) :- volvo_c30, !.
 
-hypothesize(unknown).             /* no idea... */
+hypothesize(ford_mondeo) :- ford_mondeo, !.
+hypothesize(peugeot_607) :- peugeot_607, !.
+hypothesize(bmw_3er) :- bmw_3er, !.
+hypothesize(toyota_camry) :- toyota_camry, !.
 
-/* Car list */
+hypothesize(mercedes_c_klasse) :- mercedes_c_klasse, !.
+hypothesize(toyota_avensis) :- toyota_avensis, !.
+hypothesize(volvo_xc_70) :- volvo_xc_70, !.
 
+hypothesize(jeep_compass) :- jeep_compass, !.
+hypothesize(land_rover_freelander) :- land_rover_freelander, !.
+hypothesize(bmw_x3) :- bmw_x3, !.
+hypothesize(mitsubishi_outlander) :- mitsubishi_outlander, !.
+
+hypothesize(jeep_grand_cherokee) :- jeep_grand_cherokee, !.
+hypothesize(land_rover_discovery) :- land_rover_discovery, !.
+hypothesize(mercedes_ml_klasse) :- mercedes_ml_klasse, !.
+
+
+hypothesize(aston_martin_cygnet) :- aston_martin_cygnet, !.
+
+hypothesize(lexus_ct) :- lexus_ct, !.
+
+hypothesize(dodge_charger) :- dodge_charger, !.
+hypothesize(bmw_5er) :- bmw_5er, !.
+hypothesize(lexus_gs) :- lexus_gs, !.
+hypothesize(volvo_s80) :- volvo_s80, !.
+
+hypothesize(chrysler_300c) :- chrysler_300c, !.
+hypothesize(jaguar_xf) :- jaguar_xf, !.
+hypothesize(mercedes_e_klasse) :- mercedes_e_klasse, !.
+
+hypothesize(porsche_maccan) :- porsche_maccan, !.
+hypothesize(infiniti_ex) :- infiniti_ex, !.
+
+hypothesize(dodge_ram) :- dodge_ram, !.
+hypothesize(bmw_x6) :- bmw_x6, !.
+hypothesize(toyota_land_cruiser) :- toyota_land_cruiser, !.
+
+
+hypothesize(tesla_model_s) :- tesla_model_s, !.
+hypothesize(mercedes_s_klasse) :- mercedes_s_klasse, !.
+hypothesize(lexus_ls) :- lexus_ls, !.
+
+hypothesize(porsche_cayenne) :- porsche_cayenne, !.
+hypothesize(infiniti_qx) :- infiniti_qx, !.
+
+hypothesize(chevrolet_camaro) :- chevrolet_camaro, !.
+hypothesize(jaguar_f_type) :- jaguar_f_type, !.
+hypothesize(bentley_continental) :- bentley_continental, !.
+hypothesize(nissan_gtr) :- nissan_gtr, !.
+
+
+hypothesize(unknown). /* no idea... */
+
+
+/******************************************/
+/*                Car list                */
+/******************************************/
 /* With low price */
-
 /* mini */
 ford_ka :- low_price_mini, american.
 peugeot_107 :- low_price_mini, french.
@@ -44,8 +132,8 @@ nissan_juke :- low_price_crossover, japan.
 /* suv */
 renault_duster :- low_price_suv, french.
 
-/* With medium price */
 
+/* With medium price */
 /* mini */
 mini_cooper :- medium_price_mini, british.
 smart_for_two :- medium_price_mini, german.
@@ -78,8 +166,8 @@ jeep_grand_cherokee :- medium_price_suv, american.
 land_rover_discovery :- medium_price_suv, british.
 mercedes_ml_klasse :- medium_price_suv, japan.
 
-/* With high price */
 
+/* With high price */
 /* mini */
 aston_martin_cygnet :- high_price_mini, british.
 
@@ -106,8 +194,8 @@ dodge_ram :- high_price_suv, american.
 bmw_x6 :- high_price_suv, german.
 toyota_land_cruiser :- high_price_suv, japan.
 
-/* With very high price */
 
+/* With very high price */
 /* sedan */
 tesla_model_s :- very_high_price_sedan, american.
 mercedes_s_klasse :- very_high_price_sedan, german.
@@ -124,22 +212,50 @@ bentley_continental :- very_high_price_coupe, german.
 nissan_gtr :- very_high_price_coupe, japan.
 
 
-/* Classification by manufacturer country */
-american :- confirmed(american).
-british :- confirmed(british).
-french :- confirmed(french).
-german :- confirmed(german).
-japan :- confirmed(japan).
-swedish :- confirmed(swedish).
-
-/* Classification by price */
+/******************************************/
+/*         Classification by price        */
+/******************************************/
 low_price :- confirmed(low_price).
 medium_price :- confirmed(medium_price).
 high_price :- confirmed(high_price).
 very_high_price :- confirmed(very_high_price).
 
 
-/* Classification by price and body type */
+/******************************************/
+/*      Classification by body type       */
+/******************************************/
+mini :- confirmed(very_small_size),
+        confirmed(for_every_day),
+        not(confirmed(for_family)).
+
+hatchback :- confirmed(small_size),
+             confirmed(for_every_day),
+             confirmed(for_family).
+
+sedan :- confirmed(medium_size),
+         confirmed(for_every_day).
+
+wagon :- confirmed(medium_size),
+         confirmed(for_family).
+
+crossover :- confirmed(large),
+             confirmed(for_every_day).
+
+suv :- confirmed(very_large_size),
+       confirmed(for_off_road),
+       confirmed(for_big_family).
+
+pickup :- confirmed(very_large_size),
+          confirmed(for_off_road),
+          confirmed(big_trunk).
+
+coupe :- confirmed(small_size),
+         not(confirmed(for_every_day)).
+
+
+/******************************************/
+/* Classification by price and body type  */
+/******************************************/
 /* low price body type */
 low_price_mini :- low_price, mini.
 low_price_hatchback :- low_price, hatchback.
@@ -174,38 +290,19 @@ very_high_price_suv :- very_high_price, suv.
 very_high_price_coupe :- very_high_price, coupe.
 
 
-/* Body type classification */
-
-mini :- confirmed(very_small_size),
-        confirmed(for_every_day),
-        not(confirmed(for_family)).
-
-hatchback :- confirmed(small_size),
-             confirmed(for_every_day),
-             confirmed(for_family).
-
-sedan :- confirmed(medium_size),
-         confirmed(for_every_day).
-
-wagon :- confirmed(medium_size),
-         confirmed(for_family).
-
-crossover :- confirmed(large),
-             confirmed(for_every_day).
-
-suv :- confirmed(very_large_size),
-       confirmed(for_off_road),
-       confirmed(for_big_family).
-
-pickup :- confirmed(very_large_size),
-          confirmed(for_off_road),
-          confirmed(big_trunk).
-
-coupe :- confirmed(small_size),
-         not(confirmed(for_every_day)).
+/******************************************/
+/* Classification by manufacturer country */
+/******************************************/
+american :- confirmed(american).
+british :- confirmed(british).
+french :- confirmed(french).
+german :- confirmed(german).
+japan :- confirmed(japan).
+swedish :- confirmed(swedish).
 
 
-/* how to ask questions */
+
+/* Main logic */
 ask(Question) :-
     write('Do you want a car with feature '),
     write(Question),
